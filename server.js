55b9173b260ddd145e1f8415e8acbf892eb2912d
Tcar <thomasrcarlin@gmail.com>
2019-06-21 02:15:40 -0400

    mongooo

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newspaperdb";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
